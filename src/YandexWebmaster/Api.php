2af8ff3ef601b2de313c8e28ffb2b0b09beaba4a
sb15 <alexander.perov@gmail.com>
2016-09-20 21:57:35 +0300

    update v3

--- src/YandexWebmaster/Api.php
+++ src/YandexWebmaster/Api.php
@@ -3,45 +3,61 @@
 namespace Sb\YandexWebmaster;
 
 use Sb\Browser\Console as Browser;
+use Sb\YandexWebmaster\Exception\Exception;
 
 class Api
 {
-    const END_POINT = 'https://webmaster.yandex.ru';
+    const END_POINT = 'https://api.webmaster.yandex.net';
 
     protected $siteId = null;
     protected $oauthToken = null;
     protected $browser = null;
+
+    private $userId = null;
 
     public function __construct($oauthToken)
     {
         $this->oauthToken = $oauthToken;
         $this->browser = new Browser();
         $this->browser->addHeader("Authorization", "OAuth " . $oauthToken);
+        $this->browser->addHeader("Accept", "application/json");
+        $this->browser->addHeader("Content-type", "application/json");
+    }
+
+    public function getUserId()
+    {
+        if (!$this->userId) {
+            $response = $this->browser->get(self::END_POINT . '/v3/user/');
+            $response = json_decode($response);
+            $this->userId = $response->user_id;
+        }
+
+        return $this->userId;
     }
 
     public function getHosts()
     {
-        $response = $this->browser->get(self::END_POINT . '/api/v2/hosts');
-        return xmlstr_to_array($response);
+        $response = $this->browser->get(self::END_POINT . '/v3/user/' . $this->getUserId() . '/hosts/');
+        return json_decode($response);
     }
 
-    public function getHost($siteId)
+    public function getHost($hostId)
     {
-        $response = $this->browser->get(self::END_POINT . '/api/v2/hosts/' . $siteId);
-        return xmlstr_to_array($response);
+        $response = $this->browser->get(self::END_POINT . '/v3/user/' . $this->getUserId() . '/hosts/' . $hostId .'/');
+        return json_decode($response);
     }
 
-    public function addOriginalText($siteId, $text)
+    public function addOriginalText($hostId, $text)
     {
-        $text = "<original-text>
-                    <content>{$text}</content>
-                </original-text>";
+        $content = json_encode(['content' => $text]);
 
-        $response = $this->browser->post(self::END_POINT . '/api/v2/hosts/'.$siteId.'/original-texts/', $text);
-        $response = xmlstr_to_array($response);
-        if (isset($response['@root']) && $response['@root'] == 'error') {
-            throw new Exception\Exception($response['message']);
+        $response = $this->browser->post(self::END_POINT . '/v3/user/' . $this->getUserId() . '/hosts/' . $hostId . '/original-texts/', $content);
+        $response = json_decode($response);
+
+        if (isset($response->error_message)) {
+            throw new Exception($response->error_message);
         }
-        return $response['id'];
+
+        return $response->text_id;
     }
 }
